359b41750c02d6a8b78117c5055f08e9ab02d73a
Jens Reidel <adrian@travitia.xyz>
2021-09-04 21:59:59 +0200

    use pure sync code

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,47 @@
-use log::info;
+use log::{error, info};
 use nadylib::{
     models::{Channel, Message},
     packets::{
         BuddyAddPacket, BuddyRemovePacket, LoginSelectPacket, MsgPrivatePacket,
         OutPrivgrpInvitePacket,
     },
-    AOSocket, ReceivedPacket, Result, SocketConfig,
+    sync_client_socket::{AOSocket, SocketConfig},
+    ReceivedPacket, Result,
 };
 
-async fn run() -> Result<()> {
-    let char_name = std::env::var("CHARNAME").expect("CHARNAME not set");
-    let username = std::env::var("USERNAME").expect("USERNAME not set");
-    let password = std::env::var("PASSWORD").expect("PASSWORD not set");
-    let mut sock = AOSocket::connect("chat.d1.funcom.com:7105", SocketConfig::default()).await?;
+fn get_var(key: &str) -> String {
+    std::env::var(key).unwrap_or_else(|_| {
+        error!("{} not set", key);
+        std::process::exit(1)
+    })
+}
 
-    while let Ok(packet) = sock.read_packet().await {
+fn main() -> Result<()> {
+    let _ = dotenv::dotenv();
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info");
+    }
+    env_logger::init();
+
+    let char_name = get_var("CHARNAME");
+    let username = get_var("USERNAME");
+    let password = get_var("PASSWORD");
+    let mut sock = AOSocket::connect("chat.d1.funcom.com:7105", SocketConfig::default())?;
+
+    while let Ok(packet) = sock.read_packet() {
         match packet {
             ReceivedPacket::LoginSeed(s) => {
-                sock.login(&username, &password, &s.login_seed).await?;
+                sock.login(&username, &password, &s.login_seed)?;
             }
             ReceivedPacket::LoginCharlist(c) => {
                 let character = c.characters.iter().find(|i| i.name == char_name).unwrap();
                 let pack = LoginSelectPacket {
                     character_id: character.id,
                 };
-                sock.send(pack).await?;
+                sock.send(pack)?;
             }
             ReceivedPacket::LoginOk => info!("Logged in successfully"),
+            ReceivedPacket::LoginError(e) => error!("Failed to login: {}", e.message),
             ReceivedPacket::MsgPrivate(m) => {
                 if m.message.text == "register" || m.message.text == "!register" {
                     let id = m.message.sender.unwrap();
@@ -34,12 +49,12 @@ async fn run() -> Result<()> {
                         character_id: id,
                         send_tag: String::from("\u{1}"),
                     };
-                    sock.send(pack).await?;
+                    sock.send(pack)?;
                     info!("User {} registered and has been added as a buddy", id);
                 } else if m.message.text == "unregister" || m.message.text == "!unregister" {
                     let id = m.message.sender.unwrap();
                     let pack = BuddyRemovePacket { character_id: id };
-                    sock.send(pack).await?;
+                    sock.send(pack)?;
                     info!("User {} unregistered and has been removed as a buddy", id);
                 } else if m.message.text == "help" || m.message.text == "!help" {
                     let pack = MsgPrivatePacket {
@@ -50,14 +65,14 @@ async fn run() -> Result<()> {
                             send_tag: String::from("\u{0}"),
                         }
                     };
-                    sock.send(pack).await?;
+                    sock.send(pack)?;
                 }
             }
             ReceivedPacket::BuddyStatus(s) if s.online => {
                 let pack = OutPrivgrpInvitePacket {
                     character_id: s.character_id,
                 };
-                sock.send(pack).await?;
+                sock.send(pack)?;
                 info!("User {} has logged on and has been invited", s.character_id);
             }
             ReceivedPacket::PrivgrpClijoin(j) => {
@@ -74,15 +89,4 @@ async fn run() -> Result<()> {
     }
 
     Ok(())
-}
-
-fn main() -> Result<()> {
-    let _ = dotenv::dotenv();
-    env_logger::init();
-
-    tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-        .block_on(run())
 }
